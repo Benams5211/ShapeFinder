3c131691c0adcc74d3860b70fca78c61d529260a
Ben <127755097+Benams5211@users.noreply.github.com>
2025-10-13 14:11:55 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -232,7 +232,7 @@ function drawMenu() {
     textAlign(CENTER, CENTER);
     textSize(48);
     textFont(pixelFont);
-    text("THAT TIME I GOT REINCARNATED INTO A NEW WORLD\n AND USED MY LEVEL 100 FLASHLIGHT SKILLS TO FIND THE WANTED SHAPE!", width/2, height/2 - 75);
+    text("THAT TIME I GOT REINCARNATED INTO A NEW WORLD\nAND USED MY LEVEL 100 FLASHLIGHT SKILLS TO FIND THE WANTED SHAPE!", width/2, height/2 - 75);
     imageMode(CORNER);
     textFont('Arial');
   } else {
@@ -883,5 +883,6 @@ function windowResized() {
     backToMenuButton.y = height / 2 + 80;
   }
 }
+
 
 
