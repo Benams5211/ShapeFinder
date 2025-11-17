da3aa0ea219719405e4fc86022675a5d59ca4179
Ben <127755097+Benams5211@users.noreply.github.com>
2025-11-16 19:33:56 -0500

    Fix test case for ClickRect containment

--- tests/clickArea-test.mjs
+++ tests/clickArea-test.mjs
@@ -14,7 +14,7 @@ const runner = new TestRunner();
 
 runner.test('We can properly click inside a rectangle ', function() {
     const rect = new ClickRect(0,0,10,10);
-    this.assertTrue(rect.contains(2,2) , '(2,2) should be inside this rectangle');
+    this.assertTrue(rect.contains(2,25) , '(2,2) should be inside this rectangle');
 });
 
 // Auto-run tests in Node.js, but not in browser (browser imports explicitly)
